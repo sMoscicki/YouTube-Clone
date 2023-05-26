0f38b5ad2ef724996fed88c19f702637f47ef8f2
sMoscicki <121615393+sMoscicki@users.noreply.github.com>
2023-05-26 15:26:31 +0200

    Update Constants.swift

--- YouTube-Clone/Helpers/Constants.swift
+++ YouTube-Clone/Helpers/Constants.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct Constants {
     
-    static let API_KEY = "AIzaSyBIUl79iyVcxyXmWRfSGp5uyRigGZq8c2w"
+    static let API_KEY = ""
     static let API_URL = "https://www.googleapis.com/youtube/v3"
     static let PLAYLIST_ID = "Mvxg0c-kjQE&list=PLu1NXqt4iKA_3GcnaVBPYZZOU_TKjIvXZ"
     
